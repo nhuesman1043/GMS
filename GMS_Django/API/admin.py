9ccc38a070ee2edb604827dfdcf6917a6bd9792c
ahoeft0 <ashtonhoeft7@gmail.com>
2024-04-23 13:45:05 -0500

    Added models and connected them to the admin page. Also created superuser.

--- GMS_Django/API/admin.py
+++ GMS_Django/API/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from API.models import Person, Plot, Plot_Status
 
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Plot)
+admin.site.register(Plot_Status)
--- GMS_Django/API/models.py
+++ GMS_Django/API/models.py
@@ -12,15 +12,25 @@ class Person(models.Model):
     portrait_image = models.ImageField()
     landscape_image = models.ImageField()
 
+    def __str__(self):
+        return self.person_id
+
 class Plot_Status(models.Model):
     status_id = models.BigAutoField(primary_key=True)
     status_name = models.CharField(max_length=50, null=False)
     color_hex = models.CharField(max_length=6, null=False)
     description = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.status_id
+
 class Plot(models.Model):
     plot_id = models.BigAutoField(primary_key=True)
-    plot_latitude = models.DecimalField(null=False)
-    plot_longitude = models.DecimalField(null=False)
-    plot_state = models.ForeignKey(Plot_Status, on_delete=models.SET_NULL)
-    person_id = models.ForeignKey(Person, on_delete=models.SET_NULL)
+    plot_identifier = models.CharField(max_length=50, default=None)
+    plot_latitude = models.DecimalField(null=False, decimal_places=8, max_digits=12)
+    plot_longitude = models.DecimalField(null=False, decimal_places=8, max_digits=12)
+    plot_state = models.ForeignKey(Plot_Status, on_delete=models.SET_NULL, null=True)
+    person_id = models.ForeignKey(Person, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.plot_id
