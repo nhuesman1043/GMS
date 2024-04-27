5764335ec92bc44124793a0b0ab741d14f3d4502
nhuesman1043 <noah.huesman@gmail.com>
2024-04-26 23:12:42 -0500

    Polished user sidebar content for dynamic picture use and overall better look.

--- GMS_Angular/src/app/sidebar/user-sidebar-content/user-sidebar-content.component.ts
+++ GMS_Angular/src/app/sidebar/user-sidebar-content/user-sidebar-content.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgIf } from '@angular/common';
+import { NgIf, NgStyle, DatePipe, formatDate } from '@angular/common';
 import { APIService } from '../../services/api.service';
 import { GlobalService } from '../../services/global.service';
 import { SidebarService } from '../../services/sidebar.service';
@@ -9,6 +9,8 @@ import { SidebarService } from '../../services/sidebar.service';
   standalone: true,
   imports: [
     NgIf,
+    DatePipe,
+    NgStyle
   ],
   templateUrl: './user-sidebar-content.component.html',
   styleUrl: './user-sidebar-content.component.scss'
@@ -22,14 +24,29 @@ export class UserSidebarContentComponent implements OnInit{
   landscapeImage: any;
 
   async getUserContentData(plotId: Number): Promise<void> {
-    // Call the getData method of ApiService to fetch plot data and then person data based on plot's person_id value
-    this.plotData = await this.apiService.getData('plot/' + plotId + '/');
-    this.personData = await this.apiService.getData('person/' + this.plotData.person_id + '/');
+    try {
+      // Call the getData method of ApiService to fetch plot data and then person data based on plot's person_id value
+      this.plotData = await this.apiService.getData('plot/' + plotId + '/');
+      this.personData = await this.apiService.getData('person/' + this.plotData.person_id + '/');
 
-    // Get images based on API_URL/imageURL and their file name
-    const imageURL = this.globalService.API_URL + 'image';
-    this.portraitImage = imageURL + this.personData.portrait_image;
-    this.landscapeImage = imageURL + this.personData.landscape_image;
+      // Get images based on API_URL/imageURL and their file name - Set to null and then check
+      // Initialize image URLs to null before fetching new ones
+      this.portraitImage = null;
+      this.landscapeImage = null;
+
+      // Build imageURL based on API_URL and 'image'
+      const imageURL = this.globalService.API_URL + 'image';
+
+      // Check if there is an available portait image and if so, set value
+      if (this.personData.portrait_image !== null)
+        this.portraitImage = imageURL + this.personData.portrait_image;
+
+      // Check if there is an available landscape image and if so, set value
+      if (this.personData.landscape_image !== null)
+        this.landscapeImage = imageURL + this.personData.landscape_image;
+    } catch(err) {
+      console.error('Error fetching user content data:', err);
+    }
   }
 
   ngOnInit() {
@@ -37,5 +54,11 @@ export class UserSidebarContentComponent implements OnInit{
       // Call getUserContentData method when sidebar is toggled
       this.getUserContentData(id);
     });
+  }
+
+  formatDateRange(startDate: Date, endDate: Date): string {
+    const formattedStartDate = formatDate(startDate, 'mediumDate', 'en-US');
+    const formattedEndDate = formatDate(endDate, 'mediumDate', 'en-US');
+    return `${formattedStartDate} - ${formattedEndDate}`;
   }
 }
