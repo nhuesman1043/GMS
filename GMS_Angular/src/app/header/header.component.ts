88f9097e47e1606c9272ed8241be3a07830ed10c
ahoeft0 <ashtonhoeft7@gmail.com>
2024-04-28 15:56:35 -0500

    Finished Info Popup

--- GMS_Angular/src/app/header/header.component.ts
+++ GMS_Angular/src/app/header/header.component.ts
@@ -1,32 +1,25 @@
-import { Component, inject, EventEmitter, HostListener, Output } from '@angular/core';
+import { Component, inject, TemplateRef,  EventEmitter, HostListener, Output, Input, SimpleChanges, SimpleChange, OnChanges, OnInit } from '@angular/core';
 import { GlobalService } from '../services/global.service';
-import { InfoPopupService } from '../info-popup/info-popup.service';
 import { WeatherWidgetComponent } from '../weather-widget/weather-widget.component';
-import { InfoPopupComponent } from '../info-popup/info-popup.component';
 import { MatDialog, MatDialogConfig, MatDialogModule } from '@angular/material/dialog';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, ModalDismissReasons, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [WeatherWidgetComponent,
     MatDialogModule,
-    InfoPopupComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   host: {ngSkipHydration: 'true'},
-  styles: '.light-blue-backdrop {background-color: #5cb3fd;}'
 })
 export class HeaderComponent {
-  constructor(private dialog: MatDialog) {}
   private modalService = inject(NgbModal);
 
-  openModel(){
-    console.log('popup started');
-
-        this.dialog.open(InfoPopupComponent, { backdropClass: 'light-blue-backdrop' });
-        console.log('popup ended');
-   }
-  
+	open(content: TemplateRef<any>) {
+		this.modalService.open(content, { backdrop: 'static', 
+                                      animation: true, 
+                                      centered: true }).result.then();
+	}
 }
