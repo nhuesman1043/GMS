376a6c7c91de09dc09b9c4f1af08dc6f3ccaf770
ahoeft0 <ashtonhoeft7@gmail.com>
2024-04-28 16:18:47 -0500

    Code cleanup and commenting for header

--- GMS_Angular/src/app/header/header.component.ts
+++ GMS_Angular/src/app/header/header.component.ts
@@ -1,16 +1,12 @@
-import { Component, inject, TemplateRef,  EventEmitter, HostListener, Output, Input, SimpleChanges, SimpleChange, OnChanges, OnInit } from '@angular/core';
-import { GlobalService } from '../services/global.service';
+import { Component, inject, TemplateRef } from '@angular/core';
 import { APIService } from '../services/api.service';
 import { WeatherWidgetComponent } from '../weather-widget/weather-widget.component';
-import { MatDialog, MatDialogConfig, MatDialogModule } from '@angular/material/dialog';
-import { NgbModal, ModalDismissReasons, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [WeatherWidgetComponent,
-    MatDialogModule,
-  ],
+  imports: [WeatherWidgetComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   host: {ngSkipHydration: 'true'},
@@ -19,6 +15,7 @@ export class HeaderComponent {
   private modalService = inject(NgbModal);
   constructor(private apiService: APIService) {}
 
+  //Open info popup view
 	open(content: TemplateRef<any>) {
 		this.modalService.open(content, { backdrop: 'static', 
                                       animation: true, 
--- GMS_Angular/src/app/weather-widget/weather-widget.component.ts
+++ GMS_Angular/src/app/weather-widget/weather-widget.component.ts
@@ -13,6 +13,7 @@ export class WeatherWidgetComponent implements OnInit {
 
   constructor() { }
 
+  //Initialize the data
   ngOnInit() {
     this.WeatherData = {
       main : {},
@@ -21,15 +22,14 @@ export class WeatherWidgetComponent implements OnInit {
     this.getWeatherData();
   }
 
+  //Get the weather data from the .json file
   getWeatherData() {
     fetch('https://api.openweathermap.org/data/2.5/weather?id=5017185&appid=b6a6ff094d3db9361ffff3c775fc6e65&units=imperial')
     .then(response=>response.json())
     .then(data=>{this.setWeatherData(data);})
-
-    // let data = JSON.parse('{"coord":{"lon":10.99,"lat":44.34},"weather":[{"id":500,"main":"Rain","description":"light rain","icon":"10d"}],"base":"stations","main":{"temp":284.24,"feels_like":283.34,"temp_min":283.18,"temp_max":284.64,"pressure":1011,"humidity":74,"sea_level":1011,"grnd_level":943},"visibility":10000,"wind":{"speed":1.24,"deg":61,"gust":1.1},"rain":{"1h":0.4},"clouds":{"all":100},"dt":1714145597,"sys":{"type":2,"id":2004688,"country":"IT","sunrise":1714104829,"sunset":1714155208},"timezone":7200,"id":3163858,"name":"Zocca","cod":200}');
-    // this.setWeatherData(data);
   }
 
+  //Calculate the different weather information to be used in the component
   setWeatherData(data: any) {
     this.WeatherData = data;
     let sunsetTime = new Date(this.WeatherData.sys.sunset * 1000);
